aa4cb381b5ff82093c9e4a0593556c7fe2ad7c57
Rajeem Cariazo <rcariazo@prophecyinternational.com>
2024-05-02 11:09:44 +0800

    Fix integration tests

--- LibraryMgmtSystem.IntegrationTest/FlatFileRepositoryTests.cs
+++ LibraryMgmtSystem.IntegrationTest/FlatFileRepositoryTests.cs
@@ -12,12 +12,12 @@ namespace LibraryMgmtSystem.IntegrationTest
 
             var repo = new FlatFileRepository();
             
-            repo.Add(new Book { Id = 1, Author = "F. Scott Fitzgerald", Title = "The Great Gatsby", Isbn = "978-0743273565" });
+            repo.Add(new Book { Id = "f3cfc4f8-c174-447d-8e2c-b674f4c81c4d", Author = "F. Scott Fitzgerald", Title = "The Great Gatsby", Isbn = "978-0743273565" });
 
-            var book = repo.Get<Book>(1);
+            var book = repo.Get<Book>("f3cfc4f8-c174-447d-8e2c-b674f4c81c4d");
 
             Assert.NotNull(book);
-            Assert.Equal(1, book.Id);
+            Assert.Equal("f3cfc4f8-c174-447d-8e2c-b674f4c81c4d", book.Id);
             Assert.Equal("F. Scott Fitzgerald", book.Author);
             Assert.Equal("The Great Gatsby", book.Title);
             Assert.Equal("978-0743273565", book.Isbn);
@@ -30,13 +30,13 @@ namespace LibraryMgmtSystem.IntegrationTest
 
             var repo = new FlatFileRepository();
 
-            repo.Add(new Book { Id = 1, Author = "J.K. Rowling", Title = "Harry Potter and the Sorcerer's Stone", Isbn = "978-0590353427" });
-            repo.Update(new Book { Id = 1, Author = "F. Scott Fitzgerald", Title = "The Great Gatsby", Isbn = "978-0743273565" });
+            repo.Add(new Book { Id = "f3cfc4f8-c174-447d-8e2c-b674f4c81c4d", Author = "J.K. Rowling", Title = "Harry Potter and the Sorcerer's Stone", Isbn = "978-0590353427" });
+            repo.Update(new Book { Id = "f3cfc4f8-c174-447d-8e2c-b674f4c81c4d", Author = "F. Scott Fitzgerald", Title = "The Great Gatsby", Isbn = "978-0743273565" });
 
-            var book = repo.Get<Book>(1);
+            var book = repo.Get<Book>("f3cfc4f8-c174-447d-8e2c-b674f4c81c4d");
 
             Assert.NotNull(book);
-            Assert.Equal(1, book.Id);
+            Assert.Equal("f3cfc4f8-c174-447d-8e2c-b674f4c81c4d", book.Id);
             Assert.Equal("F. Scott Fitzgerald", book.Author);
             Assert.Equal("The Great Gatsby", book.Title);
             Assert.Equal("978-0743273565", book.Isbn);
